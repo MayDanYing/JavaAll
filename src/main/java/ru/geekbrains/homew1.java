005fae8fbd17592c0962ad9191a542d1f6d4ccc8
MayDanYing <tasya757@gmail.com>
2020-10-14 13:58:00 +0300

    initial commit

---
+++ src/main/java/ru/geekbrains/homew1.java
@@ -0,0 +1,34 @@
+package ru.geekbrains;
+import java.util.Random;
+
+public class homew1 {
+
+
+            public static void main(String[] args)
+        {
+            int []arr = new int[10];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = rand.nextInt( 10);
+
+            int MIN = arr[0];
+            int MAX = arr[0];
+
+            for (int j : arr)
+                System.out.print(j + "\t");
+
+            System.out.println();
+
+            for (int j : arr) {
+                if (MAX <= j)
+                    MAX = j;
+                if (MIN >= j)
+                    MIN = j;
+            }
+            System.out.println("MAX = " + MAX);
+            System.out.println("MIN = " + MIN);
+        }
+    }
+

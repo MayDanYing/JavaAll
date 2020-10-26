fae2cdcc0b105db322d857eb11d50053add0475a
MayDanYing <tasya757@gmail.com>
2020-10-26 12:52:20 +0300

    c

--- src/main/java/ru/geekbrains/lesson6/SizeException.java
+++ src/main/java/ru/geekbrains/lesson6/SizeException.java
@@ -3,7 +3,7 @@ public class SizeException extends Exception {
     int n, m;
 
     public SizeException(int n, int m) {
-        super("Неправильно ввели количество строк и столбцов. Массив должен быть 4на4. Вы ввели " + n + ", column " + m);
+        super("Неправильно ввели количество строк и столбцов. Массив должен быть 4на4. Вы ввели строк" + n + ", столбцов " + m);
         this.n = n;
         this.m = m;
     }

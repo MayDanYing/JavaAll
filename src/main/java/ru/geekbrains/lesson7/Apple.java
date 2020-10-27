b9616871291bff48344ad9698997febf522b5487
MayDanYing <tasya757@gmail.com>
2020-10-27 19:07:16 +0300

    c

---
+++ src/main/java/ru/geekbrains/lesson7/Apple.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.lesson7;
+
+//Даны классы Fruit, Apple extends Fruit,
+
+public final class Apple extends Fruit {
+    public Apple() {
+        super(1.0f);
+    }
+}
---
+++ src/main/java/ru/geekbrains/lesson7/Orange.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.lesson7;
+
+// Даны классы Fruit, Apple extends Fruit, Orange extends Fruit;
+
+public final class Orange extends Fruit {
+    public Orange() {
+        super(1.5f);
+    }
+}
